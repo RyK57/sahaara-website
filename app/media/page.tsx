023079edeb12b208e5b52063ecfae593ca34bf78
RyK57 <rithviks.ryk57@gmail.com>
2026-07-06 01:11:51 -0700

    (refactor) simplify media page shell layout

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,5 +1,3 @@
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
 import { MediaGallery } from "./_components/media-gallery";
 
 export const metadata = {
@@ -10,9 +8,7 @@ export const metadata = {
 export default function MediaPage() {
   return (
     <div className="min-h-screen flex flex-col">
-      <main className="flex-1">
-        <MediaGallery />
-      </main>
+      <MediaGallery />
     </div>
   );
 }
