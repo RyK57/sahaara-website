b72c9aead917b4eb30501d174f7d609794ba2933
RyK57 <rithviks.ryk57@gmail.com>
2026-07-06 01:11:51 -0700

    (refactor) simplify resources page shell layout

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,5 +1,3 @@
-import { Header } from "@/components/header";
-import { Footer } from "@/components/footer";
 import { ResourcesContent } from "./_components/resources-content";
 
 export const metadata = {
@@ -11,11 +9,7 @@ export const metadata = {
 export default function ResourcesPage() {
   return (
     <div className="min-h-screen flex flex-col">
-      <main className="flex-1">
-        <div className="max-w-8xl mx-auto w-full px-8">
-          <ResourcesContent />
-        </div>
-      </main>
+      <ResourcesContent />
     </div>
   );
 }
